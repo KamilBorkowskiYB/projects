df7aabc13954286d3aa3be22953dae555b6f9bb0
Kamil <kamil.borkow@wp.pl>
2023-09-23 18:53:53 +0200

    Added function that calculates area of pizza

--- PYTHON/PizzaPriceReworked/main.py
+++ PYTHON/PizzaPriceReworked/main.py
@@ -4,6 +4,13 @@ root=tk.Tk()
 root.geometry('600x600')
 root.title('Pizza Worth Calc')
 
+def calculation():
+    d = pizza_D.get(1.0, "end-1c")
+    p = pizza_price.get(1.0, "end-1c")
+    area = (float(d)/2)*(float(d)/2)*3.14
+    print(area)
+    print(float(d)+float(p))
+
 label_d = tk.Label(root,text = "Diameter of pizza")
 label_d.pack()
 pizza_D = tk.Text(root,height=2, width=20, font=("Arial",24))
@@ -12,7 +19,7 @@ label_p = tk.Label(root, text = "Pizza's price")
 label_p.pack()
 pizza_price = tk.Text(root,height=2, width=20, font=("Arial",24))
 pizza_price.pack()
-btn_calc = tk.Button(root, text="Calculate")
+btn_calc = tk.Button(root, text="Calculate", command=lambda: calculation())
 btn_calc.pack()
 
 
