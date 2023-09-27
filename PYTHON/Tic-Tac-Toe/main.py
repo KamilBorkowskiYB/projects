fd18884eed888337c9234a625403e4e57c061b20
Kamil <kamil.borkow@wp.pl>
2023-09-27 18:32:04 +0200

    Added text that states state of the game

--- PYTHON/Tic-Tac-Toe/main.py
+++ PYTHON/Tic-Tac-Toe/main.py
@@ -16,19 +16,21 @@ def btn_state(b):
                 b['text'] = 'X'
                 moves += 1
                 XorO = 0
+                info['text'] = 'O on move'
             elif XorO == 0:
                 b['text'] = 'O'
                 moves += 1
                 XorO = 1
+                info['text'] = 'X on move'
 
 
 def who_won(b):
     global play_on
     if b['text'] == 'O':
-        print("O won")
+        info['text'] = 'O won'
         play_on = False
     if b['text'] == 'X':
-        print("X won")
+        info['text'] = 'X won'
         play_on = False
 
 
@@ -50,29 +52,37 @@ def win():
     if (btn_7['text'] == btn_8['text'] == btn_9['text']) and btn_7['text'] != " ":
         who_won(btn_7)
     elif moves >= 9:
-        print("Draw")
+        info['text'] = 'Draw'
 
 
 root = tk.Tk()
 root.geometry('375x600')
 root.title('Tic-Tac-Toe')
 
-btn_1 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_1), win()], height=2, width=5, font=("Arial", 30))
+game_frame = tk.Frame(root)
+game_frame.pack()
+
+btn_1 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_1), win()], height=2, width=5, font=("Arial", 30))
 btn_1.grid(row=0, column=0)
-btn_2 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_2), win()], height=2, width=5, font=("Arial", 30))
+btn_2 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_2), win()], height=2, width=5, font=("Arial", 30))
 btn_2.grid(row=0, column=1)
-btn_3 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_3), win()], height=2, width=5, font=("Arial", 30))
+btn_3 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_3), win()], height=2, width=5, font=("Arial", 30))
 btn_3.grid(row=0, column=2)
-btn_4 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_4), win()], height=2, width=5, font=("Arial", 30))
+btn_4 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_4), win()], height=2, width=5, font=("Arial", 30))
 btn_4.grid(row=1, column=0)
-btn_5 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_5), win()], height=2, width=5, font=("Arial", 30))
+btn_5 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_5), win()], height=2, width=5, font=("Arial", 30))
 btn_5.grid(row=1, column=1)
-btn_6 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_6), win()], height=2, width=5, font=("Arial", 30))
+btn_6 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_6), win()], height=2, width=5, font=("Arial", 30))
 btn_6.grid(row=1, column=2)
-btn_7 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_7), win()], height=2, width=5, font=("Arial", 30))
+btn_7 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_7), win()], height=2, width=5, font=("Arial", 30))
 btn_7.grid(row=2, column=0)
-btn_8 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_8), win()], height=2, width=5, font=("Arial", 30))
+btn_8 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_8), win()], height=2, width=5, font=("Arial", 30))
 btn_8.grid(row=2, column=1)
-btn_9 = tk.Button(root, text=" ", command=lambda: [btn_state(btn_9), win()], height=2, width=5, font=("Arial", 30))
+btn_9 = tk.Button(game_frame, text=" ", command=lambda: [btn_state(btn_9), win()], height=2, width=5, font=("Arial", 30))
 btn_9.grid(row=2, column=2)
+
+info = tk.Label(root, text="X on move", font=("Arial", 30), pady=30)
+info.pack()
+btn_res = tk.Button(root, text="Restart", width=10, font=("Arial", 24))
+btn_res.pack()
 root.mainloop()
