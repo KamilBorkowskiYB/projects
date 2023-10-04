fc70e25d344a145d3be3705345f2491908d2b737
Kamil <kamil.borkow@wp.pl>
2023-10-04 15:56:47 +0200

    Finished menu frame

--- PYTHON/Math_Quiz/main.py
+++ PYTHON/Math_Quiz/main.py
@@ -1,37 +1,47 @@
+import tkinter as tk
 import ttkbootstrap as ttk
 
 win = ttk.Window()
-win.geometry('300x300')
+win.geometry('900x900')
 win.title('Math Quiz')
 
 
 def start_quiz():
     frame_start.pack_forget()
-    frame_game.pack()
+    frame_game.pack(expand=True, fill='both')
 
 
 # frames
 frame_start = ttk.Frame(win)
 frame_game = ttk.Frame(win)
 
+# styles
+style = ttk.Style()
+style.configure('.', font=('Helvetica', 48))
+style.configure('my.TButton', font=('Helvetica', 150))
+
 # start menu widgets
-menu_line1 = ttk.Label(frame_start, text='You will answer 10 math question in a limited time', anchor="center")
-menu_line2 = ttk.Label(frame_start, text='Are you ready?', anchor="center")
-btn_play = ttk.Button(frame_start, text='Play', command=lambda: start_quiz())
+menu_line1 = ttk.Label(frame_start, text='You will answer 10 math\nquestion in a limited time.\nAre you ready?', anchor="center", justify="center")
+btn_play = ttk.Button(frame_start, text='Play', command=lambda: start_quiz(), style='my.TButton')
 
 # game widgets
 question_label = ttk.Label(frame_game, text='Question 1')
+equation_label = ttk.Label(frame_game, text='Equation 1')
+answer_entry = ttk.Entry(frame_game)
+next_question = ttk.Button(frame_game, text='Next Question')
 
 # menu grid
 frame_start.columnconfigure(0, weight=1)
-frame_start.rowconfigure([0,1,2], weight=1)
+frame_start.rowconfigure([0,1], weight=1)
 frame_start.pack(expand=True,fill='both')
 menu_line1.grid(row = 0, column = 0, sticky='nswe')
-menu_line2.grid(row = 1, column = 0, sticky='nswe')
-btn_play.grid(row = 2, column = 0, sticky='NSEW')
+btn_play.grid(row = 1, column = 0, sticky='NSEW')
 
 # game grid
-frame_game.pack()
-question_label.pack()
+frame_game.pack(expand=True)
+question_label.pack(pady=(50,0))
+equation_label.pack()
+next_question.pack(side='bottom',pady=(0,90))
+answer_entry.pack(side='bottom')
 frame_game.pack_forget()
 win.mainloop()
