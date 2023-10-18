4b2f615a8f8f129aff7215db3402b36aafa609fc
Kamil <kamil.borkow@wp.pl>
2023-10-18 18:42:30 +0200

    Program calculates all possibilities of ships config

--- PYTHON/Battleships/main.py
+++ PYTHON/Battleships/main.py
@@ -1,32 +1,101 @@
+#             2x,3x,4x,5x
+sunken_ships = [0,0,0,0]
+
+
 def show(x):
    for i in x:
        print(i)
 
 
 def seek_target():
-    pass
+    global sunken_ships
+    global pos_2x
+    global pos_3x
+    global pos_4x
+    global pos_5x
+    global sum_pos
+    if sunken_ships[0] == 0:
+        calculate_matrices(2)
+    if sunken_ships[1] == 0:
+        calculate_matrices(3)
+        for k in range(10):
+            for l in range(10):
+                pos_3x[k][l] =pos_3x[k][l]*2
+    if sunken_ships[1] == 1:
+        calculate_matrices(3)
+    if sunken_ships[2] == 0:
+        calculate_matrices(4)
+    if sunken_ships[3] == 0:
+        calculate_matrices(5)
+
+    for i in range(10):
+        for j in range(10):
+            sum_pos[i][j] = pos_2x[i][j]+pos_3x[i][j]+pos_4x[i][j]+pos_5x[i][j]
 
 
 def calculate_matrices(length):
     global pos_2x
-    global pos_3x_1
+    global pos_3x
     global pos_4x
     global pos_5x
+    global hits
     if length == 2:
         for i in range(10):
             for j in range(10-length+1):
-                pos_2x[i][j] += 1
-                pos_2x[i][j+1] += 1
+                if hits[i][j] == 0 and hits[i][j+1] == 0:
+                    pos_2x[i][j] += 1
+                    pos_2x[i][j+1] += 1
         for k in range(10-length+1):
             for l in range(10):
-                pos_2x[k][l] += 1
-                pos_2x[k+1][l] += 1
+                if hits[k][l] == 0 and hits[k+1][l] == 0:
+                    pos_2x[k][l] += 1
+                    pos_2x[k+1][l] += 1
     elif length == 3:
-        pass
+        for i in range(10):
+            for j in range(10 - length + 1):
+                if hits[i][j] == 0 and hits[i][j + 1] == 0 and hits[i][j + 2] == 0:
+                    pos_3x[i][j] += 1
+                    pos_3x[i][j + 1] += 1
+                    pos_3x[i][j + 2] += 1
+        for k in range(10 - length + 1):
+            for l in range(10):
+                if hits[k][l] == 0 and hits[k + 1][l] == 0 and hits[k + 2][l] == 0:
+                    pos_3x[k][l] += 1
+                    pos_3x[k + 1][l] += 1
+                    pos_3x[k + 2][l] += 1
     elif length == 4:
-        pass
-    else:
-        pass
+        for i in range(10):
+            for j in range(10 - length + 1):
+                if hits[i][j] == 0 and hits[i][j + 1] == 0 and hits[i][j + 2] == 0 and hits[i][j + 3] == 0:
+                    pos_4x[i][j] += 1
+                    pos_4x[i][j + 1] += 1
+                    pos_4x[i][j + 2] += 1
+                    pos_4x[i][j + 3] += 1
+        for k in range(10 - length + 1):
+            for l in range(10):
+                if hits[k][l] == 0 and hits[k + 1][l] == 0 and hits[k + 2][l] == 0 and hits[k + 3][l] == 0:
+                    pos_4x[k][l] += 1
+                    pos_4x[k + 1][l] += 1
+                    pos_4x[k + 2][l] += 1
+                    pos_4x[k + 3][l] += 1
+    elif length == 5:
+        for i in range(10):
+            for j in range(10 - length + 1):
+                if hits[i][j] == 0 and hits[i][j + 1] == 0 and hits[i][j + 2] == 0 and hits[i][j + 3] == 0 and hits[i][j + 4] == 0:
+                    pos_5x[i][j] += 1
+                    pos_5x[i][j + 1] += 1
+                    pos_5x[i][j + 2] += 1
+                    pos_5x[i][j + 3] += 1
+                    pos_5x[i][j + 4] += 1
+        for k in range(10 - length + 1):
+            for l in range(10):
+                if hits[k][l] == 0 and hits[k + 1][l] == 0 and hits[k + 2][l] == 0 and hits[k + 3][l] == 0 and hits[k + 4][l] == 0:
+                    pos_5x[k][l] += 1
+                    pos_5x[k + 1][l] += 1
+                    pos_5x[k + 2][l] += 1
+                    pos_5x[k + 3][l] += 1
+                    pos_5x[k + 4][l] += 1
+
 
 def target_hit():
     pass
@@ -66,7 +135,7 @@ pos_2x = [
     [0,0,0,0,0,0,0,0,0,0],
     ]
 
-pos_3x_1 = [
+pos_3x = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -135,16 +204,14 @@ hits = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,1,0,1,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,0,0,1,0],
+    [0,0,0,0,0,0,0,0,1,0],
     ]
 
-show(ships)
-print(ships[1][2])
-calculate_matrices(2)
-show(pos_2x)
+seek_target()
+show(sum_pos)
